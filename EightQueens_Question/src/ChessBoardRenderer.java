d06685a05cfb2dee475f693b1a3e4980c54e7807
JATIN RX <JATIN RX@DESKTOP-494EF0F>
2020-07-09 17:40:38 +0530

    solution done

--- EightQueens_Question/src/ChessBoardRenderer.java
+++ EightQueens_Question/src/ChessBoardRenderer.java
@@ -2,7 +2,12 @@ public class ChessBoardRenderer {
 	public boolean isBlackSquare(int square) {
 
 		// WRITE YOUR LOGIC HERE...
-		System.out.println(square);
-		return true;
+		
+		if(square % 16 < 8) {
+			if(square % 2 == 0) return true;
+		}else {
+			if(square % 2 == 1) return true;
+		}
+		 return false;
 	}
 }
--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -4,14 +4,13 @@ public class GameStrategy {
 
 	private int getColumn(int cellId) {
 		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+		return cellId % 8;
+		
 	}
 	
 	private int getRow(int cellId) {
 		// WRITE YOUR LOGIC HERE....................................
-		
-		return 0;	// just for the heck of it
+		return cellId / 8;
 	}
 
 	public boolean isValidPosition(int cellId) {
@@ -25,10 +24,33 @@ public class GameStrategy {
 		
 		int row = getRow(cellId);
 		
-		/*
-			WRITE YOUR LOGIC HERE...............................
-
-		*/
+		for(int i = 0; i < 8 ; i++) {
+			if(placedQueens[i][col]) return false;
+ 		}
+		
+		for(int i = 0; i < 8 ; i++) {
+			if(placedQueens[row][i]) return false;
+ 		}
+		
+		for(int i = row - 1,j = col - 1; i >= 0 && j >= 0; i--,j--)
+		{
+			if(placedQueens[i][j]) return false; 
+		}
+		for(int i = row - 1,j = col + 1; i >= 0 && j < 8; i--,j++)
+		{
+			if(placedQueens[i][j]) return false; 
+		}
+		for(int i = row + 1,j = col + 1; i < 8 && j < 8; i++,j++)
+		{
+			if(placedQueens[i][j]) return false; 
+		}
+		for(int i = row + 1,j = col - 1; i < 8 && j >= 0; i++,j--)
+		{
+			if(placedQueens[i][j]) return false; 
+		}
+		
+		placedQueens[row][col] = true;
+		numQueens++;
 		return isValid;
 	}
 }
